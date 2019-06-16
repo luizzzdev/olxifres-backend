4aaaa79eda55920049de017812706d3b0390cc7f
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2019-06-16 10:17:18 -0300

    pequena refatoracao leiloes controller

--- src/controllers/LeiloesController.ts
+++ src/controllers/LeiloesController.ts
@@ -4,9 +4,19 @@ export default class LeiloesController {
   async index(request, response, next) {
     let sql = 'SELECT * FROM leilao l INNER JOIN animal a ON l.id_animal = a.id_animal';
     let options = ' ORDER BY l.data';
+    const conditions: string[] = [];
 
     if (request.query.status) {
-      sql += ` WHERE l.status = ${request.query.status}`;
+      conditions.push(` l.status = ${request.query.status}`);
+    }
+
+    if (request.query.idLeilao) {
+      conditions.push(` l.id_leilao = ${request.query.idLeilao}`);
+    }
+
+    if ('status' in request.query || 'idLeilao' in request.query) {
+      sql += ' WHERE';
+      sql += conditions.join(' AND ');
     }
 
     if (request.query.limit) {
