28d150e63ee5ecd6f16f739290e25571a75fc990
Luiz Fernando <luiiz_roodrigues@hotmail.com>
2019-06-15 21:41:56 -0300

    habilitando cors

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(require('./router'));
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use(require('./router'));
 
 app.get('/healthcheck', (req, res) => res.json({ status: 'ok' }));
 
